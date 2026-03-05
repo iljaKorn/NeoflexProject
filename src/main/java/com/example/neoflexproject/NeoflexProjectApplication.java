1baff35e210388e53d048487a3d52a4a7f633028
ilyaKorn <il.kornilow@yandex.ru>
2026-03-05 13:15:47 +0300

    Initial commit

---
+++ src/main/java/com/example/neoflexproject/NeoflexProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.neoflexproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoflexProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoflexProjectApplication.class, args);
+	}
+
+}
