298f5aab88e6f08a932468ed81afa6b94da807a4
ilyaKorn <il.kornilow@yandex.ru>
2026-03-31 20:44:45 +0300

    Добавил обработку ошибок

---
+++ deal/src/main/java/com/neoproject/deal/exception/DealServiceException.java
@@ -0,0 +1,7 @@
+package com.neoproject.deal.exception;
+
+public class DealServiceException extends RuntimeException {
+    public DealServiceException(String message) {
+        super(message);
+    }
+}
